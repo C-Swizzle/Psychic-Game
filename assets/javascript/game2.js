1ee39ea9244542fe3bd197cb9f2a1a61b91f618b
Chris <chrisem816@gmail.com>
2019-01-04 14:11:59 -0800

    l

---
+++ assets/javascript/game2.js
@@ -0,0 +1,6 @@
+var win =0;
+var lose=0;
+var guessLeft=9;
+var guessSoFar=0;
+var compLetters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var alphabetLength = compLetters.length;
